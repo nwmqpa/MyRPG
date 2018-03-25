c770933b6ff4ad894b997e3bbca2fa73aa5962a6
afourcat <alexandre.fourcat@epitech.eu>
2018-03-25 16:16:30 +0200

    Adding first structure

---
+++ includes/name.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2017
+** name.c
+** File description:
+** Name
+*/
+
+#ifndef NAME
+#define NAME
+
+/*
+** Game: 
+** 	Main game structure that represent game state and
+** 	game datas inside all the game.
+*/
+typedef struct game_s {
+	int gamemode;
+} game_t;
+
+#endif /* NAME */
