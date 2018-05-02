56419f65b0a7591f2ead1c0d3e37bfbbc18e156c
afourcat <alexandre.fourcat@epitech.eu>
2018-05-02 23:15:15 +0000

    Adding popup

--- include/ui.h
+++ include/ui.h
@@ -8,9 +8,28 @@
 #ifndef UI_H_
 #define UI_H_
 
+#define FONT_SIZE 24
+
 #include <SFML/Graphics.h>
 
-void ui_draw(sfRenderWindow *win, player_t *player);
-void ui_update(player_t *player);
+struct popup {
+	char *string;
+	sfText *text;
+	sfSprite *sprite;
+	sfTexture *texture;
+	int pause;
+	int value;
+	float time;
+};
+
+//string is the text that will be print in the popup
+struct popup *popup_create(char *string);
+
+//pause is time between fading
+void popup_animate(sfRenderWindow *win, struct popup *pop, int pause);
+
+//If string == NULL keep old string
+//If pause == NULL keep old pause
+void popup_reset(struct popup *pop, char *string, int pause);
 
 #endif
