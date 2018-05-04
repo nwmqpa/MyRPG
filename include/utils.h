edebe4063d786d2ebde77b62b434182dc2779f5d
Thomas "nwmqpa" Nicollet <thomas.nicollet@epitech.eu>
2018-05-04 16:41:34 +0200

    Make cleaned

--- include/utils.h
+++ include/utils.h
@@ -10,13 +10,13 @@
 
 #include <stddef.h>
 
-int my_nblen(int nb);
 void *my_malloc(size_t size);
 int my_strlen(const char *str);
 char *my_strdup(char const *str);
-void *my_calloc(size_t size);
-char *my_strdup(const char *);
+char *my_strcat(char *src1, char const *src2);
+char **split_string(char const *str);
 int my_atoi(char const *str);
+void *my_calloc(size_t size);
 float get_dtime(float time);
 
 #endif /* UTILS_H_ */
