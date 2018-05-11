fcd2de9476e41132dd7c928979a1bd1e131b9dfb
afourcat <alexandre.fourcat@epitech.eu>
2018-05-11 13:31:01 +0200

    Adding skippable cinematique

--- include/cin_mng.h
+++ include/cin_mng.h
@@ -23,5 +23,6 @@ typedef struct cinematique cin_t;
 cin_t *cin_create(float time, char *string, sfFont *font, sfColor color);
 void cin_update(sfRenderWindow *win, cin_t *cin);
 void cin_reset(cin_t *cin);
+int cin_play(sfRenderWindow *win, cin_t *cin);
 
 #endif /* CIN_MNG_H_ */
